413020e9ef97c33820c33cde7337d9b95f84fd70
bstoecker <bernhard.stoecker@invision.de>
2013-12-19 10:13:35 +0100

    some specs

--- src/models/employee.erl
+++ src/models/employee.erl
@@ -1,5 +1,5 @@
 -module(employee).
--export([new/1, new_list/1, fields/0, to_struct_list/1]).
+-export([new/1, new_list/1, fields/0, to_struct_list/1, get/2]).
 
 -record(employee, {id,first_name,last_name}).
 
@@ -8,7 +8,7 @@ new(PropList) ->
 
 new_list(PropListList) -> new_list([], PropListList).
 
-new_list(Employees, []) -> Employees;
+new_list(Employees, []) -> lists:reverse(Employees);
 new_list(Employees, [Head|SubPropListList]) ->
   new_list([new(Head)|Employees], SubPropListList).
 
@@ -19,3 +19,21 @@ to_struct_list(EmployeeList) ->
 
 fields() -> record_info(fields, employee).
 
+get(Employee, Attribute) ->
+  Index = find_field_index(Attribute),
+  element(Index, Employee).
+
+
+% Internal
+
+find_field_index(Attribute) ->
+  find_field_index(record_info(fields, employee), Attribute).
+
+find_field_index(Fields, Attribute) -> find_field_index(Fields, Attribute, 2).
+
+find_field_index([], _, _) -> erlang:error(badarg);
+find_field_index([Field| Other], Attribute, Index) ->
+  case Field =:= Attribute of
+    true -> Index;
+    false -> find_field_index(Other, Attribute, Index + 1)
+  end.
--- test/employee_tests.erl
+++ test/employee_tests.erl
@@ -2,13 +2,35 @@
 -include_lib("eunit/include/eunit.hrl").
 
 new_test() ->
-  out:puts("It should return an empty employee record"),
-  Args = [
-    {<<"id">>, 42},
-    {<<"first_name">>, "Hanna"},
-    {<<"last_name">>, "Annah"}
-  ],
+  Args = [{id, 42},{first_name, "Hanna"},{last_name, "Annah"}],
   Employee = employee:new(Args),
-  ?assertEqual(Employee#employee.id, 42),
-  ?assertEqual(Employee#employee.first_name, "Hanna"),
-  ?assertEqual(Employee#employee.last_name, "Annah").
+  ?assertEqual(42, employee:get(Employee, id)),
+  ?assertEqual("Hanna", employee:get(Employee, first_name)),
+  ?assertEqual("Annah", employee:get(Employee, last_name)).
+
+new_list_test() ->
+  Args = [
+    [{id, 42},{first_name, "Hanna"},{last_name, "Annah"}],
+    [{id, 43},{first_name, "Shawn"},{last_name, "Sheep"}]
+  ],
+  [Employee1, Employee2] = employee:new_list(Args),
+  ?assertEqual(42, employee:get(Employee1, id)),
+  ?assertEqual("Hanna", employee:get(Employee1, first_name)),
+  ?assertEqual("Annah", employee:get(Employee1, last_name)),
+  ?assertEqual(43, employee:get(Employee2, id)),
+  ?assertEqual("Shawn", employee:get(Employee2, first_name)),
+  ?assertEqual("Sheep", employee:get(Employee2, last_name)).
+
+to_struct_list_test() ->
+  Args = [
+    [{id, 42},{first_name, "Hanna"},{last_name, "Annah"}],
+    [{id, 43},{first_name, "Shawn"},{last_name, "Sheep"}]
+  ],
+  EmployeeList = employee:new_list(Args),
+  Converted = employee:to_struct_list(EmployeeList),
+  ?assertEqual(length(Converted), 2).
+
+
+
+
+  % to_struct_list
